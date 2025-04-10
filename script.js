4eca4dabec09edd48eea25314eb43cd1405f9883
viictoriaheloisa <victoria.teixeira2@fatec.sp.gov.br>
2025-04-10 11:28:09 -0300

    Exercício dos Botões

---
+++ script.js
@@ -0,0 +1,36 @@
+function exemplo1() {
+    document.getElementById("titulo").textContent = "Bem-vindo à minha página!";
+    alert("Bem-vindo à minha página!");
+  }
+  
+  function exemplo2() {
+    alert("Script externo funcionando corretamente!");
+  }
+  
+  function exemplo3() {
+    alert("Botão foi pressionado");
+  }
+  
+  function exemplo4() {
+    let resultado = 5 + 10;
+    alert("Resultado da soma: " + resultado);
+  }
+  
+  function exemplo5() {
+    const caixa = document.getElementById("caixa");
+    caixa.style.backgroundColor = "#add8e6"; // azul claro
+  }
+  
+  function exemplo6() {
+    const novo = document.getElementById("novo");
+    alert("Conteúdo do novo parágrafo: " + novo.textContent);
+  }
+  
+  function exemplo7() {
+    document.getElementById("minhaDiv").innerHTML = "<strong>Atualizado com sucesso!</strong>";
+  }
+  
+  function exemplo8() {
+    document.getElementById("titulo").style.color = "red";
+  }
+  
